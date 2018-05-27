fff71dfd98e3c8c137487d1ee87c9422280e63db
root <root@docker0-dk2.lxd>
2018-05-27 09:54:48 +0000

    update

--- runnode-public.sh
+++ runnode-public.sh
@@ -4,7 +4,7 @@ IMGNAME="ethereum/client-go:stable"
 ETH_NET_ID="3963"
 NODE_NAME=$1
 NODE_NAME=${NODE_NAME:-"node1"}
-#NODE_NET="--public" # comment to disable public node option
+NODE_NET="--public" # comment to disable public node option
 DETACH_FLAG=${DETACH_FLAG:-"-d"}
 CONTAINER_NAME="ethereum-$NODE_NAME"
 DATA_ROOT=${DATA_ROOT:-"$(pwd)/.ether-$NODE_NAME"}
@@ -17,24 +17,12 @@ RPC_ARG=
 PORT_ARG=
 NODE_PORT="30303"
 
-
 if [[ ! -z $RPC_PORT ]]; then
     RPC_ARG='--rpc --rpcaddr=0.0.0.0 --rpcapi=db,eth,net,web3,personal --rpccorsdomain "*"'
     RPC_PORTMAP="-p $RPC_PORT:8545"
 fi
 BOOTNODE_URL=${BOOTNODE_URL:-$(./getbootnodeurl.sh)}
-if [ ! -f $(pwd)/genesis.json ]; then
-    echo "No genesis.json file found, please run 'genesis.sh'. Aborting."
-    exit
-fi
-if [ ! -d $DATA_ROOT/keystore ]; then
-    echo "$DATA_ROOT/keystore not found, running 'geth init'..."
-    docker run --rm \
-        -v $DATA_ROOT:/root/.ethereum \
-        -v $(pwd)/genesis.json:/opt/genesis.json \
-        $IMGNAME init /opt/genesis.json
-    echo "...done!"
-fi
+echo $BOOTNODE_URL
 
 # check public node option
 if [ "$NODE_NET" = "--public" ]; then
@@ -43,10 +31,8 @@ fi
 
 echo "Running new container $CONTAINER_NAME..."
 docker run $DETACH_FLAG --name $CONTAINER_NAME \
-    --network ethereum \
     -v $DATA_ROOT:/root/.ethereum \
     -v $DATA_HASH:/root/.ethash \
-    -v $(pwd)/genesis.json:/opt/genesis.json \
     $RPC_PORTMAP \
     $PORT_ARG \
     $IMGNAME --bootnodes=$BOOTNODE_URL --networkid=$ETH_NET_ID $RPC_ARG --cache=512 --verbosity=4 --maxpeers=27 ${@:2}
